63869bef7f24368f425d2c1c4f325682ccba4153
ChipchiIinka <m.gi2016@yandex.ru>
2023-11-09 21:17:50 +0300

    remake Main forEach - stream

--- src/main/java/aiteko/Main.java
+++ src/main/java/aiteko/Main.java
@@ -1,38 +1,15 @@
 package aiteko;
 
 import aiteko.Tasks_1_to_9.*;
+import java.util.stream.Stream;
 
 public class Main
 {
-    public static void main(String[] args) {
-
-        //1
-        PrimeNumbersList arrayPrimeNumbers = new PrimeNumbersList();
-        arrayPrimeNumbers.run();
-        //2
-        LuckyNumbersList arrayLuckyNumbers = new LuckyNumbersList();
-        arrayLuckyNumbers.run();
-        //3
-        RandomNumberToWord randomNumberToWord = new RandomNumberToWord();
-        randomNumberToWord.run();
-        //4
-        FermatTheorem fermatTheorem = new FermatTheorem();
-        fermatTheorem.run();
-        //5
-        CalculateTheExpression calculateTheExpression = new CalculateTheExpression();
-        calculateTheExpression.run();
-        //6
-        FibonacciNumberList arrayFibonacciNumber = new FibonacciNumberList();
-        arrayFibonacciNumber.run();
-        //7
-        PalindromeNumbersList arrayPalindromeNumbers = new PalindromeNumbersList();
-        arrayPalindromeNumbers.run();
-        //8
-        FractionalPartList arrayFractionalPart = new FractionalPartList();
-        arrayFractionalPart.run();
-        //9
-        PascalTriangle pascalTriangle = new PascalTriangle();
-        pascalTriangle.run();
-
+    public static void main( String[] args ) {
+        Stream.of(new PrimeNumbersList(), new LuckyNumbersList(),
+                new RandomNumberToWord(), new FermatTheorem(),
+                new CalculateTheExpression(), new FibonacciNumberList(),
+                new PalindromeNumbersList(), new FractionalPartList(),
+                new PascalTriangle()).forEach(CanRun::run);
     }
 }
